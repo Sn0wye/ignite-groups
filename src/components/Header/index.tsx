b352bb6e292ac234456ae98d02966888800ae460
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-11 14:35:39 -0300

    feat: add header component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,22 @@
+import logoImg from '@assets/logo.png';
+import { BackButton, BackIcon, Container, Logo } from './styles';
+
+interface Props {
+  showBackButton: boolean;
+}
+
+export const Header = ({
+  showBackButton = false
+}: Props) => {
+  
+  return (
+    <Container>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+      <Logo source={logoImg} />
+    </Container>
+  )
+}
