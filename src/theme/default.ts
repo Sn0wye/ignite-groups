1004d3ef8934da35243a1625ce164f2386e898f0
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2022-10-11 14:10:36 -0300

    feat: add theme

---
+++ src/theme/default.ts
@@ -0,0 +1,29 @@
+export const defaultTheme = {
+  colors: {
+    white: '#FFFFFF',
+
+    'green-500': '#00B37E',
+    'green-700': '#00875F',
+
+    'red-500': '#F75A68',
+    'red-700': '#AA2834',
+
+    'gray-700': '#121214',
+    'gray-600': '#202024',
+    'gray-500': '#29292E',
+    'gray-400': '#323238',
+    'gray-300': '#7C7C8A',
+    'gray-200': '#C4C4CC',
+    'gray-100': '#E1E1E6'
+  },
+  fontFamily: {
+    regular: 'Roboto_400Regular',
+    bold: 'Roboto_700Bold'
+  },
+  fontSize: {
+    sm: 14,
+    md: 16,
+    lg: 18,
+    xl: 24
+  }
+} as const;
